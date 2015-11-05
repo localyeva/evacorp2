ae9d698cd3fca303be907b56314db1840e408124
haudv <haudv@evolableasia.vn>
2015-11-05 17:01:51 +0700

    vietnam page

---
+++ public_html/html/corporate/_include/vietnam/vie.php
@@ -0,0 +1,200 @@
+<div class="container-fluid">
+    <div class="container office-con">
+        <div class="row">
+            <div class="row-gap-big"></div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <i class="blur-black"><h3>REASON:1</h3></i>
+                <div class="row-gap-medium"></div>
+            </div>
+            
+            <div class="col-xs-12 col-md-12 text-center">
+                <h2><span class="green-text">開発ならエボ開発</span>ならエボ開発ならエボ開発ならエボ</h2>
+                <div class="row-gap-small"></div>
+                <hr>
+            </div>
+            
+            <div class="col-xs-12 col-md-12">
+                <div class="row-gap-small"></div>
+                <p>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ<br/>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ
+                </p>
+                <div class="row-gap-medium"></div>                
+            </div>                     
+            
+            <div class="col-xs-12 col-md-12 text-center">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+            </div>            
+        </div>
+    </div>
+    <div class="row-gap-big"></div>
+    <div class="row-gap-medium"></div>
+</div>
+
+<div class="container-fluid bg">
+    <div class="container office-con">
+        <div class="row">
+            <div class="row-gap-big"></div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <i class="blur-black"><h3>REASON:2</h3></i>
+                <div class="row-gap-medium"></div>
+            </div>
+            
+            <div class="col-xs-12 col-md-12 text-center">
+                <h2><span class="green-text">開発ならエボ開発</span>な<span class="green-text">開発ならエボ開発</span>エボ開発ならエボ</h2>
+                <div class="row-gap-small"></div>
+                <hr>
+            </div>
+            
+            <div class="col-xs-12 col-md-12">
+                <div class="row-gap-small"></div>
+                <p>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ<br/>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ
+                </p>
+                <div class="row-gap-medium"></div>                
+            </div>
+            
+            <div class="col-xs-4 col-md-4">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+                <div class="row-gap-small"></div>
+                <p>発ならエボ開発ならエボ</p>
+            </div>
+            <div class="col-xs-4 col-md-4">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+                <div class="row-gap-small"></div>
+                <p>発ならエボ開発ならエボ</p>
+            </div>
+            <div class="col-xs-4 col-md-4">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+                <div class="row-gap-small"></div>
+                <p>発ならエボ開発ならエボ</p>
+            </div>
+        </div>
+    </div>
+    <div class="row-gap-big"></div>
+    <div class="row-gap-medium"></div>
+</div>
+<div class="container-fluid">
+    <div class="container office-con">
+        <div class="row">
+            <div class="row-gap-big"></div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <i class="blur-black"><h3>REASON:3</h3></i>
+                <div class="row-gap-medium"></div>
+            </div>
+            
+            <div class="col-xs-12 col-md-12 text-center">
+                <h2>ならエボ開発ならエボ開発ならエボ<span class="green-text">開発ならエボ開発</span></h2>
+                <h2><span class="green-text">開発ならエボ開発</span>ならエボ開発ならエボ開発ならエボ</h2>
+                <div class="row-gap-small"></div>
+                <hr>
+            </div>
+            
+            <div class="col-xs-12 col-md-12">
+                <div class="row-gap-small"></div>
+                <p>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ 開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ 開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ 開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発ならエボ 開発ならエボ開発ならエボ開発ならエボ開発ならエボ開発
+                </p>
+                <div class="row-gap-medium"></div>                
+            </div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <div class="row-gap-small"></div>
+                <h3 style=" text-decoration: underline">ならエボ開発ならエボ開</h3>
+                <div class="row-gap-medium"></div>                
+            </div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+            </div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <div class="row-gap-big"></div>
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">                
+            </div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <div class="grey-bg">
+                    <div class="row-gap-small"></div>
+                    <h3 class="green-text">発ならエボ開発ならエボ開発ならエボ開発ならエボ開発な</h3>
+                    <div class="row-gap-small"></div>
+                    <p>発ならエボ開発ならエボ開発ならエボ開発ならエボ開</p>
+                    <div class="row-gap-small"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row-gap-big"></div>
+    <div class="row-gap-medium"></div>
+</div>
+<div class="container-fluid bg">
+    <div class="container office-con">
+        <div class="row">
+            <div class="row-gap-big"></div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <i class="blur-black"><h3>REASON:4</h3></i>
+                <div class="row-gap-medium"></div>
+            </div>
+            
+            <div class="col-xs-12 col-md-12 text-center">
+                <h2>ならエボ開発ならエボ開発ならエボ<span class="green-text">開発ならエボ開発</span></h2>
+                <div class="row-gap-small"></div>
+                <hr>
+            </div>
+            
+            <div class="col-xs-12 col-md-12">
+                <div class="row-gap-small"></div>
+                <p>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ<br/>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ
+                </p>
+                <div class="row-gap-medium"></div>                
+            </div>
+            
+            <div class="col-xs-6 col-md-6">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+            </div>
+            <div class="col-xs-6 col-md-6">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+                <p>開発ならエボ開発ならエボ開発な</p>
+            </div>
+        </div>
+    </div>
+    <div class="row-gap-big"></div>
+    <div class="row-gap-medium"></div>
+</div>
+<div class="container-fluid">
+    <div class="container office-con">
+        <div class="row">
+            <div class="row-gap-big"></div>
+            <div class="col-xs-12 col-md-12 text-center">
+                <i class="blur-black"><h3>REASON:5</h3></i>
+                <div class="row-gap-medium"></div>
+            </div>
+            
+            <div class="col-xs-12 col-md-12 text-center">
+                <h2><span class="green-text">開発ならエボ開発</span>ならエボ開発ならエボ開発ならエボ</h2>
+                <div class="row-gap-small"></div>
+                <hr>
+            </div>
+            
+            <div class="col-xs-12 col-md-12">
+                <div class="row-gap-small"></div>
+                <p>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ<br/>
+                    開発ならエボ開発ならエボ開発ならエボ開発ならエボ
+                </p>
+                <div class="row-gap-medium"></div>                
+            </div>
+            
+            <div class="col-xs-4 col-md-4 text-center">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+            </div>
+            <div class="col-xs-4 col-md-4 text-center">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+            </div>
+            <div class="col-xs-4 col-md-4 text-center">
+                <img src="img/12.png" alt="" class="img-responsive full-width main-img">
+            </div>
+        </div>
+    </div>
+    <div class="row-gap-big"></div>
+    <div class="row-gap-medium"></div>
+</div>

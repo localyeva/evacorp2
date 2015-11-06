1e6e8bcd52a809f62b17eced997ff45f45678b2c
haudv <haudv@evolableasia.vn>
2015-11-06 10:17:54 +0700

    vietnam page

--- git-evacorp2.sh
+++ git-evacorp2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-lessc /var/www/html/evacorp2/public_html/html/corporate/css/style.less > /var/www/html/evacorp2/public_html/html/corporate/css/style.css
-cp /var/www/html/evacorp2/public_html/html/corporate/css/style.css /var/www/html/evacorp2/public_html/wp-content/themes/eva2015/css/style.css
+lessc /var/www/evacorp2/public_html/html/corporate/css/style.less > /var/www/evacorp2/public_html/html/corporate/css/style.css
+cp /var/www/evacorp2/public_html/html/corporate/css/style.css /var/www/evacorp2/public_html/wp-content/themes/eva2015/css/style.css

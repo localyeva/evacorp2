e4d85fe5e42996f5095eca634be62eaec651284f
Nguyen Hoang Vu <vunh@cpu-temp.evolableasia.local>
2015-11-19 08:55:43 +0700

    update 19/11

--- public_html/html/corporate/_include/bpo/bpo.php
+++ public_html/html/corporate/_include/bpo/bpo.php
@@ -1,78 +1,72 @@
-<div class="container-fluid center bpo-1">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr text-center">
-                <span class="text-1">Evolable Asia demo demo demo demo</span><br>
-                <span class="text-2">Demo Demo Demo Demo Demo Demo Demo</span>
-            </div>
+<div class="containe center bpo-1">
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr text-center">
+            <span class="text-1">Evolable Asia demo demo demo demo</span><br>
+            <span class="text-2">Demo Demo Demo Demo Demo Demo Demo</span>
         </div>
-        <div class="row-gap-huge"></div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr">
-                <div class="col-xs-12 col-md-12 no-padding-lr">
-                    <img src="img/57.jpg" alt="" class="img-responsive img-reason-1 center-block">
-                </div>
-            </div>
-        </div>
-        <div class="row-gap-huge"></div>
     </div>
-</div>
-<div class="container-fluid center bpo-2">
-    <div class="container">
-        <div class="row-gap-huge"></div>
-        <div class="row-gap-huge"></div>
-        <div class="row-gap-huge"></div>
-        <div class="row-gap-huge"></div>
+    <div class="row-gap-huge"></div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr">
+            <div class="col-xs-12 col-md-12 no-padding-lr">
+                <img src="img/57.jpg" alt="" class="img-responsive img-reason-1 center-block">
+            </div>
+        </div>
     </div>
+    <div class="row-gap-huge"></div>
 </div>
-<div class="container-fluid center bpo-3">
-    <div class="container">
-        <div class="row-gap-huge"></div>
-        <div class="row-gap-medium"></div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr text-center">
-                <span class="text-1">ACTUAL CONTENTS</span><br>
-                <span class="text-3">BPO Demo Demo </span>
-                <div class="drop-line-top center-block"></div>
-            </div>
+<div class="container center bpo-2">
+    <div class="row-gap-huge"></div>
+    <div class="row-gap-huge"></div>
+    <div class="row-gap-huge"></div>
+    <div class="row-gap-huge"></div>
+</div>
+<div class="container center bpo-3">
+    <div class="row-gap-huge"></div>
+    <div class="row-gap-medium"></div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr text-center">
+            <span class="text-1">ACTUAL CONTENTS</span><br>
+            <span class="text-3">BPO Demo Demo </span>
+            <div class="drop-line-top center-block"></div>
         </div>
-        <div class="row-gap-big"></div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr">
-                <img src="img/58.jpg" alt="" class="img-responsive img-reason-1 center-block">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr">
-                <div class="bs-callout">
-                    <span class="text-2">Demo Demo Demo Demo</span>
-                </div>
-                <div class="drop-line"></div>
-                <img src="img/59.jpg" alt="" class="img-responsive img-1 center-block">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr">
-                <div class="bs-callout">
-                    <span class="text-2">Demo Demo Demo Demo</span>
-                </div>
-                <div class="drop-line"></div>
-                <img src="img/60.jpg" alt="" class="img-responsive img-1 center-block">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-md-12 no-padding-lr">
-                <div class="bs-callout">
-                    <span class="text-2">Demo Demo Demo Demo</span>
-                </div>
-                <div class="drop-line"></div>
-                <img src="img/61.jpg" alt="" class="img-responsive img-1 center-block">
-            </div>
-        </div>
-        <div class="row-gap-huge"></div>
     </div>
+    <div class="row-gap-big"></div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr">
+            <img src="img/58.jpg" alt="" class="img-responsive img-reason-1 center-block">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr">
+            <div class="bs-callout">
+                <span class="text-2">Demo Demo Demo Demo</span>
+            </div>
+            <div class="drop-line"></div>
+            <img src="img/59.jpg" alt="" class="img-responsive img-1 center-block">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr">
+            <div class="bs-callout">
+                <span class="text-2">Demo Demo Demo Demo</span>
+            </div>
+            <div class="drop-line"></div>
+            <img src="img/60.jpg" alt="" class="img-responsive img-1 center-block">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 no-padding-lr">
+            <div class="bs-callout">
+                <span class="text-2">Demo Demo Demo Demo</span>
+            </div>
+            <div class="drop-line"></div>
+            <img src="img/61.jpg" alt="" class="img-responsive img-1 center-block">
+        </div>
+    </div>
+    <div class="row-gap-huge"></div>
 </div>
-<div class="container-fluid center bpo-4">
+<div class="container center bpo-4">
     <div class="row-gap-giant"></div>
     <div class="row">
         <div class="col-xs-12 col-md-3 no-padding-lr left-1"></div>

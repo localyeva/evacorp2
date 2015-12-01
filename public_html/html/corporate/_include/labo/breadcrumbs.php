011dd973abfa6e65bbb1b872f1830ff33c34e6fc
haudv <haudv@evolableasia.vn>
2015-12-01 14:51:18 +0700

    breadcrumb of labo page

--- public_html/html/corporate/_include/labo/breadcrumbs.php
+++ public_html/html/corporate/_include/labo/breadcrumbs.php
@@ -3,12 +3,12 @@
     <div class="container">
         <ul class="breadcrumb-cp">
             <li>
-                <a href="#">HOME</a> <span class="divider">></span>
-            </li>
-            <li>
-                <a href="#">About EvolableAsia</a> <span class="divider">></span>
-            </li>
-            <li class="active">LPO</li>
+                    <a href="#">HOME</a> <span class="divider">></span>
+                </li>
+                <li>
+                    <a href="#">サービス</a> <span class="divider">></span>
+                </li>
+                <li class="active">ラボ型オフショア開発</li>
         </ul>
     </div>
 </div>

76aad57ea79ecab6f82929e43d40eea41fb42542
Kantoh <leduongkhang@gmail.com>
2015-12-02 17:01:25 +0700

    create default eva template

---
+++ public_html/wp-content/themes/eva2015/page-default-eva.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Author: KhangLe
+ * Template Name: Default Eva
+ * 
+ */
+get_header();
+?>
+<style>
+    .video-container {
+        height: 0;
+        overflow: hidden;
+        padding-bottom: 56.25%;
+        padding-top: 30px;
+        position: relative;
+    }
+    .video-container iframe, .video-container object, .video-container embed {
+        height: 100%;
+        left: 0;
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+</style>
+<div id="company-profile">
+
+    <?php echo $post->post_content; ?>
+
+    <?php get_template_part('part', 'contact'); ?>
+
+</div>
+<?php get_footer() ?>

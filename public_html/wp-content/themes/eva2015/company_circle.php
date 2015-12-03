60945f5f1593f9c8e2096c97d0315125cbad61e4
Kantoh <leduongkhang@gmail.com>
2015-12-03 08:50:40 +0700

    update text

--- public_html/wp-content/themes/eva2015/company_circle.php
+++ public_html/wp-content/themes/eva2015/company_circle.php
@@ -9,7 +9,7 @@ get_header();
 <section id="circle-profile">
     <div class="header-circle-banner">
         <div class="container text-center">
-            <h2>Welcome Evolable Asia</h2>
+            <h2>取り組み紹介</h2>
         </div>
     </div>
     <div class="row-gap-medium"></div>

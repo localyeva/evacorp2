5f6a464813a7f423b96e29aa78dcb81c23f4a49f
Ly Vinh Truong <truonglv@evolableasia.vn>
2015-12-09 15:02:43 +0700

    Fix CSS of news page

--- public_html/wp-content/themes/eva2015/archive-news.php
+++ public_html/wp-content/themes/eva2015/archive-news.php
@@ -2,9 +2,10 @@
 /*
  * Author: KhangLe
  * Template Name: Arichive-News
- * 
+ *
  */
 get_header();
+print_r($_SERVER);
 ?>
 
 <section>
@@ -23,27 +24,49 @@ get_header();
                         'paged' => $paged,
                     );
                     $wp_query->query($param);
-                    if ($wp_query->have_posts()):while ($wp_query->have_posts()) : $wp_query->the_post();
-                            ?>
-                            <li>
-                                <div class="txtNews" id="news<?php the_ID(); ?>">
-                                    <h3>
-                                        <span class="t0b5"><?php the_time('Y.m.d'); ?></span>
-                                        <span class="fz18"><?php the_title(); ?></span>
-                                    </h3>
-                                </div>
-                                <div class="descNews"><?php the_content(); ?></div>
-                            </li>
-                        <?php endwhile;
-                    endif; ?>
-                </ul>
-                <?php if (function_exists('wp_pagenavi')) {
-                    wp_pagenavi();
-                }; ?>
-
+                ?>
+                <?php if ($wp_query->have_posts()) { ?>
+                    <?php
+                    $i = 0;
+                    while ($wp_query->have_posts()) { ?>
+                        <?php
+                            $wp_query->the_post();
+                            $postId = get_the_ID();
+                            $terms = get_the_terms($postId, 'news-type');
+                            $categories = array();
+                            if (is_array($terms) && count($terms) > 0) {
+                                foreach ($terms as $term) {
+                                    $categories[] = $term->name;
+                                }
+                            }
+                            $post_image = get_field('image');
+                            if (($i - 3) % 3 == 0) {
+                                $color = 'pink';
+                            } elseif (($i - 1) % 3 == 0) {
+                                $color = 'yellow';
+                            } else {
+                                $color = 'sky-blue';
+                            }
+                        ?>
+                        <div class="col-md-4 item">
+                            <a href="<?php echo get_permalink(); ?>">
+                                <img src="<?php echo !empty($post_image) ? $post_image : $default_img; ?>" alt="" />
+                                <span class="info">
+                                    <span class="overlay"></span>
+                                    <span class="post-date"><?php the_time('Y.m.d'); ?></span>
+                                    <?php if (count($categories) > 0) { ?>
+                                    <span class="category <?php echo $color; ?>"><?php echo implode(', ', $categories); ?></span>
+                                    <?php } ?>
+                                    <h3 class="title"><?php the_title(); ?></h3>
+                                </span>
+                            </a>
+                        </div>
+                    <?php $i++; } ?>
+                <?php } ?>
+                <?php wp_reset_postdata(); ?>
             </div>
         </div>
-    </div>    
+    </div>
 </section>
 
 <?php get_footer(); ?>

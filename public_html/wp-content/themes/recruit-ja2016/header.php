d0e2b7eadd934cf7a59465e3051890596c25dc5d
caodinhkha <caodinhkha@gmail.com>
2015-12-24 09:40:27 +0700

    divide header and footer parts for recruit

--- public_html/wp-content/themes/recruit-ja2016/header.php
+++ public_html/wp-content/themes/recruit-ja2016/header.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ja" xml:lang="ja" xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title><?php echo wp_title('|', true, 'right') ?></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta content="text/html; charset=UTF-8" http-equiv="Content-Type"/>
-        <meta content="text/javascript" http-equiv="Content-Script-Type"/>
-        <meta content="text/css" http-equiv="Content-Style-Type"/>
         <meta name="description" content="ベトナムでオフショア開発、ラボ型オフショア開発、BPOを行なうEVOLABLE ASIA(エボラブルアジア)の求人特設サイトです。「海外で働きたい！」と思っているあなた、EVAではグローバルな視点をもつ仲間を求めています。" />
         <meta name="keywords" content="ベトナム,オフショア開発,ラボ型オフショア開発,BPO,アウトソーシング,ホーチミン,エボラブルアジア,EVOLABLE ASIA,海外,求人,海外で働く,転職,インターン " />
 
+        <title><?php echo wp_title('|', true, 'right') ?></title>
+
+        <!-- Bootstrap -->
+        <!--<link href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css" rel="stylesheet">-->
         <link href="<?php echo get_template_directory_uri() ?>/img/ico/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
-        <link href="<?php echo get_template_directory_uri() ?>/img/ico/favicon.ico" type="image/x-icon" rel="icon"/>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/import.css" type="text/css" media="all" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link href="<?php echo get_template_directory_uri() ?>/img/ico/favicon.ico" type="image/x-icon" rel="icon"/>        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link href="<?php echo get_template_directory_uri() ?>/css/jquery.sidr.dark.css" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css" type="text/css" media="all" />
+        <link href="<?php echo get_template_directory_uri() ?>/css/animate.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri() ?>/css/font-awesome.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->   
         <script type="text/javascript">
             var _gaq = _gaq || [];
             _gaq.push(['_setAccount', 'UA-47878088-1']);
@@ -38,80 +48,84 @@
                 snode.parentNode.insertBefore(lft, snode);
             })();
         </script>
-        <?php wp_head(); ?> 
+        <?php wp_head(); ?>                    
     </head>
-    <?php global $body_id; ?>
+    <?php global $body_id; ?>    
     <body id="<?php echo $body_id ?>">
-        <div id="headerWrap"> 
-            <!-- /header start -->
-            <div id="headerInner" class="clearfix">
-                <p class="floatL"> 
-                    <a href="<?php echo get_site_url(1) ?>"><img src="<?php echo get_template_directory_uri() ?>/img/header/logo.png" alt="EVOLABLE ASIA" width="156" height="51" class="opa" /></a>
-                    <a href="<?php echo bloginfo('url') ?>"><img src="<?php echo get_template_directory_uri() ?>/img/header/btn_t03.jpg" alt="日本人スタッフ募集 Recruit" width="115" height="56" class="opa ml30"/></a> 
-                </p>
-                <p class="floatR"> <a target="_blank" href="<?php echo get_site_url(1) ?>"><img src="<?php echo get_template_directory_uri() ?>/img/header/btn_t02.jpg" alt="EVOLABLE ASIA CORPORATE SITE" width="202" height="50" class="opa"/></a> <a href="<?php echo bloginfo('url') ?>/contact"><img src="<?php echo get_template_directory_uri() ?>/img/header/btn_t01.jpg" alt="応募フォーム" width="199" height="50"  class="opa" /></a> </p>
-
-                <!-- /social -->
-                <!-- /SNS start -->
-                <div class="socialWrap">
-                    <div id="SNS">
-                        <div id="SNStop">
-                            <div class="iineBtn-g">
-                                <g:plusone size="medium"></g:plusone>
-                                <script type="text/javascript">
-                                    window.___gcfg = {lang: 'ja'};
-                                    (function () {
-                                        var po = document.createElement('script');
-                                        po.type = 'text/javascript';
-                                        po.async = true;
-                                        po.src = 'https://apis.google.com/js/plusone.js';
-                                        var s = document.getElementsByTagName('script')[0];
-                                        s.parentNode.insertBefore(po, s);
-                                    })();
-                                </script>
-                            </div>	
-                            <div class="iineBtn-tw">
-                                <a href="https://twitter.com/share" class="twitter-share-button" data-lang="ja">Tweet</a>
-                                <script>!function (d, s, id) {
-                                        var js, fjs = d.getElementsByTagName(s)[0];
-                                        if (!d.getElementById(id)) {
-                                            js = d.createElement(s);
-                                            js.id = id;
-                                            js.src = "//platform.twitter.com/widgets.js";
-                                            fjs.parentNode.insertBefore(js, fjs);
-                                        }
-                                    }(document, "script", "twitter-wjs");</script>
-                            </div>
-                            <div class="iineBtn-fb">
-                                <!--<div class="fb-like" data-href="<?php echo bloginfo('url') ?>" data-send="true" data-layout="button_count" data-width="400" data-show-faces="true" data-font="arial" data-share="true"></div>-->
-                                <div class="fb-like" data-href="<?php echo bloginfo('url') ?>" data-width="400" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
-                            </div>
-
-
-                        </div>
-                    </div>
-                    <!-- /SNS end -->
+        <div class="container">
+            <div class="row head-cruit">
+                <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2 center">
+                    <a href="<?php echo get_site_url(1) ?>"><img src="<?php echo get_template_directory_uri() ?>/img/header/logo.png" alt="EVOLABLE ASIA" width="156" height="51" /></a>
                 </div>
-                <!-- /social end -->
-
+                <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1 center head-cruit-item">
+                    <a href="<?php echo bloginfo('url') ?>"><img src="<?php echo get_template_directory_uri() ?>/img/header/btn_t03.jpg" alt="日本人スタッフ募集 Recruit" width="115" height="56" /></a>
+                </div>
+                <div class="col-xs-12 col-sm-1 col-md-3 col-lg-3"></div>
+                <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 center head-cruit-item">
+                    <a href="<?php echo get_site_url(1) ?>"><img src="<?php echo get_template_directory_uri() ?>/img/header/btn_t02.jpg" alt="EVOLABLE ASIA CORPORATE SITE" width="202" height="50" /></a>
+                </div>
+                <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 center head-cruit-item">
+                    <a href="<?php echo bloginfo('url') ?>/contact"><img src="<?php echo get_template_directory_uri() ?>/img/header/btn_t01.jpg" alt="応募フォーム" width="199" height="50" /></a>
+                </div>                                                                
             </div>
-            <!-- /header end --> 
-        </div>
-        <div id="gNaviWrap"> 
-            <!-- /globalNavi start -->
-            <ul id="gNavi" class="clearfix">
-                <li id="gNavi01"><a href="<?php echo bloginfo('url') ?>/message"></a></li>
-                <li id="gNavi02"><a href="javascript:void();"></a>
-                    <ul class="ul_mchil">
-                        <li><a href="<?php echo bloginfo('url') ?>/voice/senpai"><span><i class="fa fa-play"></i>  </span>先輩社員の声</a></li>
-                        <li><a href="<?php echo bloginfo('url') ?>/voice/intern"><span><i class="fa fa-play"></i>  </span>先輩インターンの声</a></li>
+
+
+            <nav class="navbar navbar-default" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?php echo bloginfo('url') ?>/message"><span class="text-above-line">代表の想い</span><br/><span class="text-below-line">message</span></a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="text-above-line">先輩の声</span><br/><span class="text-below-line">voice</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo bloginfo('url') ?>/voice/senpai">先輩社員の声</a></li>
+                                <li><a href="<?php echo bloginfo('url') ?>/voice/intern">先輩インターンの声</a></li>
+                            </ul>
+                        </li>                        
+                        <li><a href="<?php echo bloginfo('url') ?>/attractive"><span class="text-above-line">EVAで働く魅力</span><br/><span class="text-below-line">attractive</span></a></li>                                                
+                        <li><a href="<?php echo bloginfo('url') ?>/job"><span class="text-above-line">仕事内容</span><br/><span class="text-below-line">job description</span></a></li>
+                        <li><a href="<?php echo bloginfo('url') ?>/requirements"><span class="text-above-line">募集要項</span><br/><span class="text-below-line">requiremants</span></a></li>
+                        <li><a href="<?php echo bloginfo('url') ?>/faq"><span class="text-above-line">よくある質問</span><br/><span class="text-below-line">faq</span></a></li>                                                
+                        <li><a href="<?php echo bloginfo('url') ?>/contact"><span class="text-above-line">代表の想い</span><br/><span class="text-below-line">contact</span></a></li>                        
                     </ul>
-                </li>
-                <li id="gNavi03"><a href="<?php echo bloginfo('url') ?>/attractive"></a></li>
-                <li id="gNavi04"><a href="<?php echo bloginfo('url') ?>/job"></a></li>
-                <li id="gNavi05"><a href="<?php echo bloginfo('url') ?>/requirements"></a></li>
-                <li id="gNavi06"><a href="<?php echo bloginfo('url') ?>/faq"></a></li>
-                <li id="gNavi07"><a href="<?php echo bloginfo('url') ?>/contact"></a></li>
-            </ul>
-            <!-- /globalNavi end --> 
+                </div><!-- /.navbar-collapse -->
+            </nav> 
+
+            <div class="row center social-cruit">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 social-cruit-content">
+                    <g:plusone size="medium"></g:plusone>
+                    <script type="text/javascript">
+                        window.___gcfg = {lang: 'ja'};
+                        (function () {
+                            var po = document.createElement('script');
+                            po.type = 'text/javascript';
+                            po.async = true;
+                            po.src = 'https://apis.google.com/js/plusone.js';
+                            var s = document.getElementsByTagName('script')[0];
+                            s.parentNode.insertBefore(po, s);
+                        })();
+                    </script>
+                    <a href="https://twitter.com/share" class="twitter-share-button" data-lang="ja">Tweet</a>
+                    <script>!function (d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (!d.getElementById(id)) {
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = "//platform.twitter.com/widgets.js";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }
+                        }(document, "script", "twitter-wjs");</script>
+                    <div class="fb-like" data-href="<?php echo bloginfo('url') ?>" data-width="400" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                </div>
+            </div>
         </div>
--- public_html/wp-content/themes/recruit-ja2016/message.php
+++ public_html/wp-content/themes/recruit-ja2016/message.php
@@ -1,96 +1,18 @@
 <?php
 /*
- * Author: KhangLe
+ * Author: KhaCao
  * Template Name: Message
  * 
  */
 get_header();
 ?>
 
-<div id="container" class="clearfix"> 
-    <!-- /container start -->
-    <h2 class="taC"><img src="<?php echo get_template_directory_uri() ?>/img/message/txt_m01.png" alt="代表の想い" width="1000" height="43" /></h2>
-    <!-- /mainContent start -->
-    <div id="main">
-        <div class="box_m01 clearfix"> <img src="<?php echo get_template_directory_uri() ?>/img/message/img_m01.jpg" alt="代表取締役社長 薛 悠司" width="376" height="517" class="floatL"/>
-            <div class="floatR">
-                <h4>グローバル人材として<br />
-                    一緒に成長できる仲間を求めています！</h4>
-                <p class="box_v01">代表取締役社長　薛 悠司</p>
-                <p>2010年5月に私は初めてベトナム、ホーチミンに降り立ちました。<br>
-                    その時の記憶は今なお鮮明に脳裏に焼き付いてます。<br>
-                    当時リクルートに勤めていた28歳だった私はその時に強烈にこの地に魅せられました。<br>
-                    ・街や人々から発せられる強烈な熱気、エネルギー<br>
-                    ・やる気あふれる若者<br>
-                    ・無限の可能性<br>
-                    そして2011年にエボラブルアジアを創業、2012年に会社設立にいたりました。<br>
-                    創業当時10名だったメンバーは2014年10月現在、 ホーチミン・ハノイで400名を数えるまでに成長し、
-                    ベトナムで最大の日系開発・BPO事業者となりました。<br>
-                    そして、今なお凄まじい勢いで成長し続けています。<br>
-                    その成長を共に加速していける、自身の成長も加速し続ける人材を我々は求めています。<br>
-                    「アジアを代表する開発・運用会社を作る。」というエボラブルアジアの経営理念を体現していける仲間を我々は本気で探しています！<br>
-                    ベトナムという無限の可能性とエネルギーを秘めた国で、 世界に誇れる企業を一緒に創り上げましょう！！</p>
-            </div>
-        </div>
-
-        <div class="core_value clearfix">
-            <div class="item floatL">
-                <h6>Mission Statement</h6>
-                <p>To be No.1 in providing IT software development support in Asia.</p>
-            </div>
-            <div class="item floatR">
-                <h6>Core Value</h6>
-                <ul class="ul_core">
-                    <li>Client is comrade</li>
-                    <li>Make customers satisfied</li>
-                    <li>Fun Fun Funny</li>
-                    <li>Think you'll be No.1</li>
-
-                </ul>
-
-            </div>
-        </div>
-
-        <h5>EVOLABLE ASIAが大切にしている考え</h5>
-        <ul class="ul_m01 clearfix t30b0">
-            <li>
-                <h6><img src="<?php echo get_template_directory_uri() ?>/img/message/txt_m04.png" alt="POINT.01" width="83" height="11"/></h6>
-                <p>視点が<br />
-                    グローバル視点である。</p>
-            </li>
-            <li>
-                <h6><img src="<?php echo get_template_directory_uri() ?>/img/message/txt_m02.png" alt="POINT.02" width="84" height="11"/></h6>
-                <p>多様性に対しての<br />
-                    深い理解がある。</p>
-            </li>
-            <li class="last">
-                <h6><img src="<?php echo get_template_directory_uri() ?>/img/message/txt_m03.png" alt="POINT.03" width="84" height="11"/></h6>
-                <p>主体的にクロスボーダーを<br />
-                    促進することができる。</p>
-            </li>
-        </ul>
-        <h5>EVOLABLE ASIAが求める人材</h5>
-        <div class="ul_list clearfix">
-            <ul class="t30b0 ul_m02 floatL">
-                <li class="li_01"> <span>グローバルな視点を持つ人材	</span></li>
-                <li class="li_02"><span>多様性に対しての深い理解がある人材</span></li>
-                <li class="li_03"><span>主体的にクロスボーダーを促進することが出来る人材</span></li>
-                <li class="li_04"><span>互いの価値観を尊重しあい、<br />
-                        バランス感覚に優れた人材</span></li>
-            </ul>
-            <ul class="t30b0 ul_m02 floatR">
-                <li class="li_05"><span>柔軟な発想で自ら考え行動し、<br />
-                        現状に満足しない人材</span></li>
-                <li class="li_06"><span>どんな困難なことにも果敢に挑戦し<br>
-                        辛抱強く取り組む人材	</span></li>
-                <li class="li_07"><span>人とのつながりを大切に出来る人材</span></li>
-            </ul>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <h1>ddff</h1>
         </div>
     </div>
-    
-    <?php get_template_part('contact_box') ?>
-
-    <!-- /container end --> 
 </div>
 
 <?php get_footer(); ?>

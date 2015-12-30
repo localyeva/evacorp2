aa05169d2fd2dd935f838b50e4686bbc2c02e7bf
Chau Hoang Duy <duychaufit@gmail.com>
2015-12-30 16:44:41 +0700

    Fix mobile UI

--- git-evacorp2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clear
-lessc /var/www/evacorp2/public_html/html/corporate/css/style.less > /var/www/evacorp2/public_html/html/corporate/css/style.css
-cp /var/www/evacorp2/public_html/html/corporate/css/style.css /var/www/evacorp2/public_html/wp-content/themes/eva2015/css/style.css

34b9da6724d277aaa1e1811787f42d2083ba5c7a
haudv <haudv@evolableasia.vn>
2016-01-05 09:42:17 +0700

    fix contact

--- public_html/wp-content/themes/eva2015/part-contact.php
+++ public_html/wp-content/themes/eva2015/part-contact.php
@@ -11,23 +11,23 @@
             </div>
             <div class="col-xs-12 no-padding-lr footer-contact">
                 <div class="container contact-width">
-                    <did class="row">
-                        <div class="col-xs-6 col-md-6 no-padding-lr left phone">
-                            <div class="container">
-                                <span>お電話からのご相談はこちら</span>
+                    <div class="row">
+                        <div class="col-xs-6 col-md-6 left phone">
+                            
+                                <span class="detail1-text">お電話からのご相談はこちら</span>
                                 <img src="<?php echo get_template_directory_uri() ?>/img/42.png" alt="" class="img-responsive phone-img">
                                 <span class="phone-text">(+84)8-3911-1489</span>
                                 <span class="detail-text">※日本語にて対応可能です。</span>
                                 <span class="detail-line"></span>
-                            </div>
+                            
                         </div>
-                        <div class="col-xs-6 col-md-6 no-padding-lr center-block mess">
+                        <div class="col-xs-6 col-md-6 center-block mess">
                            <a href="<?php echo bloginfo('url') ?>/contact">
-                            <div class="container">
+                            
                                 <img src="<?php echo get_template_directory_uri() ?>/img/43.png" alt="" class="img-responsive mess-img">
                                 <span class="mess-text">Webからのご相談はこちら ></span>
                                 <span class="mess-border"></span>
-                            </div>
+                            
                            </a>
                         </div>
                     </did>

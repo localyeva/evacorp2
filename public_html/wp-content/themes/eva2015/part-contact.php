82f7d2e89d823e347fc12f566e5816966dc7a62b
haudv <haudv@evolableasia.vn>
2016-01-05 13:15:41 +0700

    fix contact

--- public_html/wp-content/themes/eva2015/part-contact.php
+++ public_html/wp-content/themes/eva2015/part-contact.php
@@ -9,31 +9,23 @@
                     </p>
                 </div>
             </div>
-            <div class="col-xs-12 no-padding-lr footer-contact">
+            <div class="col-xs-12 footer-contact text-center">
                 <div class="container contact-width">
                     <div class="row">
-                        <div class="col-xs-6 col-md-6 left phone">
-                            
-                                <span class="detail1-text">お電話からのご相談はこちら</span>
-                                <img src="<?php echo get_template_directory_uri() ?>/img/42.png" alt="" class="img-responsive phone-img">
-                                <span class="phone-text">(+84)8-3911-1489</span>
-                                <span class="detail-text">※日本語にて対応可能です。</span>
-                                <span class="detail-line"></span>
-                            
+                        <div class="col-xs-12 col-md-6 center-block phone">
+                            <div class="detail1-text">お電話からのご相談はこちら</div>                                
+                            <div class="phone-text"> <img src="<?php echo get_template_directory_uri() ?>/img/42.png" alt="" class="img-responsive phone-img">(+84)8-3911-1489</div>
+                            <div class="detail-text">※日本語にて対応可能です。</div>
+                            <span class="detail-line"></span>
                         </div>
-                        <div class="col-xs-6 col-md-6 center-block mess">
-                           <a href="<?php echo bloginfo('url') ?>/contact">
-                            
-                                <img src="<?php echo get_template_directory_uri() ?>/img/43.png" alt="" class="img-responsive mess-img">
-                                <span class="mess-text">Webからのご相談はこちら ></span>
-                                <span class="mess-border"></span>
-                            
-                           </a>
+                        <div class="col-xs-12 col-md-6 center-block mess">
+                            <a href="<?php echo bloginfo('url') ?>/contact">
+                                <div class="mess-text"><img src="<?php echo get_template_directory_uri() ?>/img/43.png" alt="" class="img-responsive mess-img">Webからのご相談はこちら ></div>
+                            </a>
                         </div>
-                    </did>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 

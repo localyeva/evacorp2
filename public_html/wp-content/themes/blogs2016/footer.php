c6a4bcc4ca969a1cae588d72134dbde73600d6f2
Kantoh <leduongkhang@gmail.com>
2016-01-07 17:09:27 +0700

    add blog theme, update db

---
+++ public_html/wp-content/themes/blogs2016/footer.php
@@ -0,0 +1,6 @@
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/jquery-1.11.3.min.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>/assets/js/common.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>

4452e2105b2e73ceedc66c5b5b8302b827ed5c8e
caodinhkha <caodinhkha@gmail.com>
2016-02-19 16:40:44 +0700

    fix fb box, twitter box, google box on mobile

--- public_html/wp-content/themes/blogs2016/single.php
+++ public_html/wp-content/themes/blogs2016/single.php
@@ -104,7 +104,7 @@ $default_img = get_template_directory_uri() . '/assets/img/default-img.png';
                     <div class="sns">
                         <ul class="snsb clearfix">
 
-                            <li>
+                            <li class="hidden-xs hidden-sm">
                                 <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&amp;t=<?php the_title(); ?>"onClick="window.open(encodeURI(decodeURI(this.href)), 'sharewindow', 'width=0, height=0, personalbar=0, toolbar=0, scrollbars=1, resizable=!'); return false;">
                                     <img src="http://eva-blog.evolable.asia/wp-content/uploads/2014/11/share.jpg" alt="share" >
                                 </a>
@@ -120,7 +120,7 @@ $default_img = get_template_directory_uri() . '/assets/img/default-img.png';
                             <li><script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
                                 <g:plusone size="tall" href="<?php the_permalink(); ?>"></g:plusone>
                             </li>
-                            <li>
+                            <li class="hidden-xs hidden-sm">
                                 <a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" class="hatena-bookmark-button" data-hatena-bookmark-title="<?php the_title(); ?>｜<?php bloginfo('name'); ?>" data-hatena-bookmark-layout="vertical" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script> 
                             </li>
                         </ul>
